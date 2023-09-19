dcdab01903d78b67216a20a4200638c021b78a3a
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-19 19:40:18 -0300

    style: :lipstick: add max height slider box & slider loader

--- components/slider/slider-box.tsx
+++ components/slider/slider-box.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const SliderBox: React.FC<Props> = ({ title, children }) => {
   return (
-    <div className='w-full'>
+    <div className='w-full min-h-[398.281px]'>
       <h3 className='text-[24px] sm:text-[28px] font-medium text-primary'>{title}</h3>
       <div className='relative px-0 sm:px-4 md:px-6 lg:px-8'>
         {children}
--- components/slider/slider-loader.tsx
+++ components/slider/slider-loader.tsx
@@ -2,7 +2,7 @@ import { Loader } from '../loader'
 
 export const SliderLoader = (): JSX.Element => {
   return (
-    <div className='flex justify-center items-center w-full'>
+    <div className='flex justify-center items-center w-full min-h-[356.28px]'>
       <Loader />
     </div>
   )
