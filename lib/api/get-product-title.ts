f6a7b51ef75b458b841b6e391eb877fd0e3bf382
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-20 02:33:27 -0300

    feat: :alien: add fetch get product & get product title

--- lib/api/get-product-title.ts
+++ lib/api/get-product-title.ts
@@ -1,41 +1,17 @@
-import { Product } from '@/types/api'
-
 const query = `
   query Query($path: ID!) {
     product(path: $path) {
       title
-      quantity
-      imageUrl
-      product {
-        name
-        brand
-        alcoholicGrade
-        content
-        package
-        category
-        subCategory
-        variety
-        bitterness
-        strain
-        vineyard
-        madeIn
-      }
-      websites {
-        name
-        logo
-        url
-        price
-        bestPrice
-        average
-      }
     }
   }
 `
 interface Response {
-  product: Product
+  product: {
+    title: string
+  }
 }
 
-export const getProduct = async (path: string): Promise<Product | null> => {
+export const getProductTitle = async (path: string): Promise<string | null> => {
   const variables = {
     path
   }
@@ -55,5 +31,5 @@ export const getProduct = async (path: string): Promise<Product | null> => {
 
   const { data }: { data: Response } = await res.json()
   if (data === null) return null
-  return data.product
+  return data.product.title
 }
