6ee7436c72438f825f1e26970cc446f04745939b
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-20 10:49:44 -0300

    refactor: :fire: remove product count loading

--- components/products-count/product-count.tsx
+++ components/products-count/product-count.tsx
@@ -9,23 +9,18 @@ interface Props {
 }
 
 export const ProductCount: React.FC<Props> = ({ className }) => {
-  const [loading, setLoading] = useState(true)
   const [count, setCount] = useState(0)
 
   const { category } = useParams()
   const searchParams = useSearchParams()
 
   const fetchData = async (): Promise<void> => {
-    setLoading(true)
     setCount(await getTotalProducts(category as string, searchParams))
-    setLoading(false)
   }
 
   useEffect(() => {
     void fetchData()
   }, [searchParams])
-
-  if (loading) return <span className={className}>Cargando...</span>
 
   return (
     <span className={className}>{count} productos</span>
