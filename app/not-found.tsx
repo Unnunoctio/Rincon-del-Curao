6947e84ee55722076dc487ab74fee4cee2be35eb
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-20 02:33:41 -0300

    feat: :sparkles: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from 'next'
+import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: '404 | Rincón del Curao'
+}
+
+export default function NotFound (): React.ReactNode {
+  return (
+    <div className='flex items-center justify-center h-full'>
+      <div className='flex flex-col gap-6'>
+        <div className='flex flex-col'>
+          <span className='pb-1 text-2xl font-medium text-active'>Error 404</span>
+          <h2 className='text-4xl font-bold text-primary'>La página que buscas no existe</h2>
+          <p className='text-primary'>Este contenido no existe o fue removido.</p>
+        </div>
+        <Link
+          href='/'
+          className='px-4 py-2 w-fit font-medium text-primary text-hover rounded-md border border-primary border-hover transition-colors'
+        >
+          Ir al Home
+        </Link>
+      </div>
+    </div>
+  )
+}
