50ab3d33caccc4aedfc13b8ae219d6dfec22e803
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-20 11:34:49 -0300

    feat: :sparkles: add progress bar

---
+++ lib/provider/progress-provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Next13ProgressBar } from 'next13-progressbar'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const ProgressProvider: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      {children}
+      <Next13ProgressBar height='2px' color='#d69e2e' options={{ showSpinner: false }} showOnShallow />
+    </>
+  )
+}
