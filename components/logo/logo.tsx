a027da7999b14e8c6b1e4a9211a5e652db8afd23
Unnunoctio <rafael.carvacho.p@gmail.com>
2023-12-28 08:08:46 -0300

    perf: :wheelchair: added aria-label a home link

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const Logo: React.FC<Props> = ({ boxHeight, logoWidth, linkClass }) => {
   return (
     <div className={`${boxHeight} flex items-center`}>
-      <Link href='/' className={linkClass}>
+      <Link href='/' aria-label='Home' className={linkClass}>
         <LogoSVG className={`${logoWidth} h-auto aspect-[1078/342]`} />
       </Link>
     </div>
