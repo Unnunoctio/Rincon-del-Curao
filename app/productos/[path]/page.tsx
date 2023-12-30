9ac45099b06bd1db6ba6f2f5437ed95fe4fc9396
Unnunoctio <rafael.carvacho.p@gmail.com>
2023-12-30 15:30:50 -0300

    feat: :sparkles: added get product peticion & start product page

--- app/productos/[path]/page.tsx
+++ app/productos/[path]/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
-import { getProductTitle } from '@/lib/api'
+import { getProduct, getProductTitle } from '@/lib/api'
+import { getCookie } from '@/lib/cookies'
 import { createBreadcrumbLinks } from '@/helpers/path'
 import { Breadcrumb } from '@/components/breadcrumb'
+import { FeatureList } from '@/components/product'
 
 interface Props {
   params: { path: string }
@@ -18,12 +21,21 @@ export const generateMetadata = async ({ params }: Props): Promise<Metadata> => 
 }
 
 export default async function ProductPage ({ params }: Props): Promise<JSX.Element> {
-  const title = 'Este es un buen producto xd'
+  const prefWebs = getCookie('prefWebs')
+  const product = await getProduct(params.path, (prefWebs === null) ? [] : prefWebs.split(','))
 
   return (
     <>
-      <Breadcrumb links={createBreadcrumbLinks(['Home', 'Cervezas', title])} />
-      <h1>{params.path}</h1>
+      <Breadcrumb links={createBreadcrumbLinks(['Home', product.category, product.title])} />
+      <h1 className='my-4 text-2xl xs:text-3xl font-medium text-primary'>{product.title}</h1>
+      <div className='flex justify-between'>
+        <section>
+          <Image src={product.image} alt={product.title} width={600} height={600} className='rounded-lg' priority />
+        </section>
+        {/* Caracteristicas */}
+        <FeatureList {...product} />
+        <section />
+      </div>
     </>
   )
 }
--- types/api.d.ts
+++ types/api.d.ts
@@ -21,3 +21,37 @@ export interface Web {
   url: string
   logo: string
 }
+
+export interface Product {
+  title: string
+  brand: string
+  quantity: number
+  alcoholicGrade: number
+  content: number
+  package: string
+  category: string
+  subCategory: string
+  madeIn: string
+  variety: string | null
+  bitterness: number | null
+  temperature: string | null
+  strain: string | null
+  vineyard: string | null
+  image: string
+  websites: Website[]
+}
+
+export interface Website {
+  name: string
+  logo: string
+  url: string
+  price: number
+  bestPrice: number
+  average: number
+  records: Record[]
+}
+
+export interface Record {
+  price: number
+  date: string
+}
