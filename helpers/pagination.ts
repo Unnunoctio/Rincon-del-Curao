6f9fe0b5090457cccb2c629abf530cfc1705e99c
Unnunoctio <rafael.carvacho.p@gmail.com>
2023-12-31 06:43:45 -0300

    fix: :bug: added total pages in generate pagination

--- helpers/pagination.ts
+++ helpers/pagination.ts
@@ -8,5 +8,9 @@ export const generatePagination = (currentPage: number, totalPages: number): any
     return [1, 2, 3, 4, 5]
   }
 
+  if (currentPage + 2 === totalPages) {
+    return [totalPages - 4, totalPages - 3, totalPages - 2, totalPages - 1, totalPages]
+  }
+
   return [currentPage - 2, currentPage - 1, currentPage, currentPage + 1, currentPage + 2]
 }
