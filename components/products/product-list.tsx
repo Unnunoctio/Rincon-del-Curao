e5275f4c16c2d66ef4d3ce809dbba68b0ca31c44
Unnunoctio <rafael.carvacho.p@gmail.com>
2023-12-31 09:24:25 -0300

    feat: :sparkles: added product list not found

--- components/products/product-list.tsx
+++ components/products/product-list.tsx
@@ -1,6 +1,7 @@
 import { getProducts } from '@/lib/api'
 import { getCookie } from '@/lib/cookies'
 import { PreviewCard } from '../card/preview-card'
+import { ProductListNotFound } from './product-list-not-found'
 
 interface Props {
   page: number
@@ -11,6 +12,8 @@ interface Props {
 export const ProductList: React.FC<Props> = async ({ page, orderBy, category }) => {
   const prefWebs = getCookie('prefWebs')
   const products = await getProducts((prefWebs === null) ? [] : prefWebs.split(','), page, orderBy, category)
+
+  if (products.length === 0) return <ProductListNotFound />
 
   return (
     <div className='grid grid-cols-product-list justify-items-center gap-8 min-h-product-list'>
