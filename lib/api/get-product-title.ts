b66977c9224890b3d105457cba7ff400d9f57969
Unnunoctio <rafael.carvacho.p@gmail.com>
2024-01-05 03:27:15 -0300

    perf: :zap: changed back request to db request (get-product-title)

--- lib/api/get-product-title.ts
+++ lib/api/get-product-title.ts
@@ -1,36 +1,63 @@
 import { IsProduct } from '@/types/api'
-
-const query = `
-  query Query($path: ID!) {
-    isProductPath(path: $path) {
-      isExist
-      title
-    }
-  }
-`
-
-interface Response {
-  isProductPath: IsProduct
-}
+import { connectDB, generatePath, generateTitle } from '../utils'
+import { ProductModel } from '../models'
 
 export const getProductTitle = async (path: string): Promise<IsProduct> => {
-  const variables = {
-    path
+  await connectDB()
+
+  const pathSplit = path.split('-')
+  const sku = parseInt(pathSplit[0])
+
+  const product: any = await ProductModel
+    .findOne({ sku })
+    .populate({ path: 'drink', select: ['alcoholic_grade', 'brand', 'content', 'name', 'package'] })
+    .exec()
+
+  if (product === null) return { isExist: false, title: null }
+
+  const productPath = generatePath(product.drink, product.quantity, product.sku)
+  if (productPath !== path) return { isExist: false, title: null }
+
+  return {
+    isExist: true,
+    title: generateTitle(product.drink.name, product.drink.package, product.drink.content, product.quantity, product.drink.alcoholic_grade)
   }
-
-  const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT as string, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': process.env.NEXT_PUBLIC_API_KEY as string
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    }),
-    cache: 'no-store'
-  })
-
-  const { data }: { data: Response } = await res.json()
-  return data.isProductPath
 }
+
+// ! Deprecated
+// import { IsProduct } from '@/types/api'
+
+// const query = `
+//   query Query($path: ID!) {
+//     isProductPath(path: $path) {
+//       isExist
+//       title
+//     }
+//   }
+// `
+
+// interface Response {
+//   isProductPath: IsProduct
+// }
+
+// export const getProductTitle = async (path: string): Promise<IsProduct> => {
+//   const variables = {
+//     path
+//   }
+
+//   const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT as string, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'x-api-key': process.env.NEXT_PUBLIC_API_KEY as string
+//     },
+//     body: JSON.stringify({
+//       query,
+//       variables
+//     }),
+//     cache: 'no-store'
+//   })
+
+//   const { data }: { data: Response } = await res.json()
+//   return data.isProductPath
+// }
