0247ea97d29d5c7b73dce02f8370e21a12fddf3b
Unnunoctio <rafael.carvacho.p@gmail.com>
2024-01-07 18:17:35 -0300

    feat: :sparkles: added lider images domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
       { protocol: 'https', hostname: 'santaisabel.vtexassets.com' },
       { protocol: 'https', hostname: 'jumbo.vtexassets.com' },
       { protocol: 'https', hostname: 'assets.jumbo.cl' },
-      { protocol: 'https', hostname: 'assets.santaisabel.cl' }
+      { protocol: 'https', hostname: 'assets.santaisabel.cl' },
+      { protocol: 'https', hostname: 'images.lider.cl' },
+      { protocol: 'https', hostname: 'www.walmartchile.cl' }
     ]
   }
 }
