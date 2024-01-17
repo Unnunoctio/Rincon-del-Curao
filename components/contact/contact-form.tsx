f17eeae9e6ee960b742ab7c5de2a1a700137e366
Unnunoctio <rafael.carvacho.p@gmail.com>
2024-01-17 03:00:22 -0300

    feat: :sparkles: added contact notification

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,53 +1,58 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 'use client'
 
+import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { Slide, ToastContainer, toast } from 'react-toastify'
 import { Areafield, Emailfield, Textfield } from '../forms'
-import { sendEmail } from '@/app/actions'
-import { useEffect, useState } from 'react'
 import { FormInput } from '@/types/types'
-
-interface Message {
-  success: boolean
-  message: string
-}
+import { sendEmail } from '@/app/actions'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const ContactForm: React.FC = () => {
   const [loading, setLoading] = useState(false)
-  const [message, setMessage] = useState<null | Message>(null)
   const { register, handleSubmit, formState: { errors }, reset } = useForm<FormInput>()
 
-  useEffect(() => {
-    if (message != null) {
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    }
-  }, [message])
+  const successNotify = (): any => toast.success('Mensaje enviado correctamente', {
+    containerId: 'success',
+    autoClose: 5000,
+    theme: 'colored',
+    transition: Slide
+  })
+  const errorNotify = (): any => toast.error('Error al enviar el mensaje', {
+    containerId: 'error',
+    autoClose: 5000,
+    theme: 'colored',
+    transition: Slide
+  })
 
   const onSubmit: SubmitHandler<FormInput> = async (data) => {
     setLoading(true)
     const success = await sendEmail(data)
     if (success) {
       reset()
-      setMessage({ success: true, message: '*Mensaje enviado correctamente' })
+      successNotify()
     } else {
-      setMessage({ success: false, message: '*Error al enviar el mensaje' })
+      errorNotify()
     }
     setLoading(false)
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='mt-6 flex flex-col gap-3'>
-      <Textfield register={register} label='Nombre' name='name' placeholder='Nombre...' required disabled={loading} error={errors.name} />
-      <Emailfield register={register} label='Email' name='email' placeholder='example@email.com' required disabled={loading} error={errors.email} />
-      <Textfield register={register} label='Asunto' name='subject' placeholder='Asunto...' required disabled={loading} error={errors.subject} />
-      <Areafield register={register} label='Mensaje' name='message' placeholder='Tú mensaje...' required disabled={loading} error={errors.message} />
+    <>
+      <ToastContainer containerId='success' position='top-right' />
+      <ToastContainer containerId='error' position='top-right' />
 
-      <button type='submit' disabled={loading} className={`${loading ? 'cursor-wait' : 'cursor-pointer'} mt-3 font-medium py-2 px-3 rounded-full bg-active/75 disabled:bg-active/50 border border-transparent text-primary transition-colors hover:bg-active`}>
-        {loading ? 'Enviando...' : 'Enviar'}
-      </button>
-      {message !== null && <span className={`${message.success ? 'text-green-500' : 'text-error'}`}>{message.message}</span>}
-    </form>
+      <form onSubmit={handleSubmit(onSubmit)} className='mt-6 flex flex-col gap-3'>
+        <Textfield register={register} label='Nombre' name='name' placeholder='Nombre...' required disabled={loading} error={errors.name} />
+        <Emailfield register={register} label='Email' name='email' placeholder='example@email.com' required disabled={loading} error={errors.email} />
+        <Textfield register={register} label='Asunto' name='subject' placeholder='Asunto...' required disabled={loading} error={errors.subject} />
+        <Areafield register={register} label='Mensaje' name='message' placeholder='Tú mensaje...' required disabled={loading} error={errors.message} />
+
+        <button type='submit' disabled={loading} className={`${loading ? 'cursor-wait' : 'cursor-pointer'} mt-3 font-medium py-2 px-3 rounded-full bg-active/75 disabled:bg-active/50 border border-transparent text-primary transition-colors hover:bg-active`}>
+          {loading ? 'Enviando...' : 'Enviar'}
+        </button>
+      </form>
+    </>
   )
 }
