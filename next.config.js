77fc49afc114c2b784bc1de90a2fe85ecf36bb02
Unnunoctio <rafael.carvacho.p@gmail.com>
2024-01-18 03:57:27 -0300

    feat: :sparkles: added cloudinary images url

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'santaisabel.vtexassets.com' },
-      { protocol: 'https', hostname: 'jumbo.vtexassets.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      // logos
       { protocol: 'https', hostname: 'assets.jumbo.cl' },
       { protocol: 'https', hostname: 'assets.santaisabel.cl' },
-      { protocol: 'https', hostname: 'images.lider.cl' },
       { protocol: 'https', hostname: 'www.walmartchile.cl' }
     ]
   }
