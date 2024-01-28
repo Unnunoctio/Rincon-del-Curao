f0d8e89f2578a9fb9fae22172ca69c5ad3b9986f
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-01-28 17:30:40 -0300

    style: :lipstick: fixed line-clamp

--- components/product/linked-product-item.tsx
+++ components/product/linked-product-item.tsx
@@ -7,7 +7,7 @@ export const LinkedProductItem: React.FC<Props> = ({ path, title, price, bestPri
   return (
     <Link href={path} className='h-fit select-none'>
       <article className='flex flex-col items-center gap-3 p-3 bg-primary rounded-lg border divider-primary hover:border-hover'>
-        <h3 className='font-medium text-[18px] text-primary text-center line-clap-2 leading-6 h-[48px]'>{title}</h3>
+        <h3 className='font-medium text-[18px] text-primary text-center line-clamp-2 leading-6 h-[48px]'>{title}</h3>
         <hr className='w-full divider-primary' />
         <div className='flex justify-center gap-3 items-baseline'>
           {price !== bestPrice && <span className='text-[16px] text-secondary line-through leading-none'>${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}</span>}
