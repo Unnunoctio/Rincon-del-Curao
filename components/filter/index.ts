697f7206c1271812621cf998815e38f0ecb3aac6
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-02-05 05:13:45 -0300

    feat: :sparkles: added multi select v2

--- components/filter/index.ts
+++ components/filter/index.ts
@@ -1,3 +1,4 @@
 export * from './multi-select'
+export * from './multi-select-v2'
 export * from './remove-filters'
 export * from './search-filter'
--- components/products/product-list-filter.tsx
+++ components/products/product-list-filter.tsx
@@ -3,7 +3,7 @@ import { FilterOptions } from '@/types/types'
 import { Suspense } from 'react'
 import { ProductCount } from '.'
 import { getCookie } from '@/lib/cookies'
-import { MultiSelect, RemoveFilters, SearchFilter } from '../filter'
+import { MultiSelectV2, RemoveFilters, SearchFilter } from '../filter'
 
 interface Props {
   category: string
@@ -26,26 +26,11 @@ export const ProductListFilter: React.FC<Props> = ({ category, optionsText, filt
       </header>
       <div className='flex flex-col gap-4 py-3'>
         <SearchFilter label='Palabras Clave' name='search' placeholder='Palabra clave...' value={filterOptions.search} aria='buscar por palabra clave' />
-        <article>
-          <span className='text-primary text-[18px] font-medium'>Categoria</span>
-          <MultiSelect name='sub_category' options={totalOptions.subCategory} ariaLabel='seleccionar categoria' />
-        </article>
-        <article>
-          <span className='text-primary text-[18px] font-medium'>Marca</span>
-          <MultiSelect name='brand' options={totalOptions.brand} ariaLabel='seleccionar marca' />
-        </article>
-        <article>
-          <span className='text-primary text-[18px] font-medium'>Contenido</span>
-          <MultiSelect name='content' options={totalOptions.content} ariaLabel='seleccionar el contenido unitario' />
-        </article>
-        <article>
-          <span className='text-primary text-[18px] font-medium'>Cantidad</span>
-          <MultiSelect name='quantity' options={totalOptions.quantity} ariaLabel='seleccionar la cantidad' />
-        </article>
-        <article>
-          <span className='text-primary text-[18px] font-medium'>Envase</span>
-          <MultiSelect name='package' options={totalOptions.package} ariaLabel='seleccionar tipo de envase' />
-        </article>
+        <MultiSelectV2 label='Categoria' name='sub_category' options={totalOptions.subCategory} aria='seleccionar categoria' />
+        <MultiSelectV2 label='Marca' name='brand' options={totalOptions.brand} aria='seleccionar marca' />
+        <MultiSelectV2 label='Contenido' name='content' options={totalOptions.content} aria='seleccionar contenido unitario' />
+        <MultiSelectV2 label='Cantidad' name='quantity' options={totalOptions.quantity} aria='seleccionar cantidad' />
+        <MultiSelectV2 label='Envase' name='package' options={totalOptions.package} aria='seleccionar envase' />
       </div>
     </section>
   )
