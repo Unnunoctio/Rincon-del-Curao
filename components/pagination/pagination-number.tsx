4b5329dbb2f393554dd7395884b95de7b97d8f87
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-02-05 21:04:36 -0300

    style: :lipstick: added bg hover in pagination button

--- components/pagination/pagination-number.tsx
+++ components/pagination/pagination-number.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const PaginationNumber: React.FC<Props> = ({ href, page, isActive, className = '' }) => {
   return (
-    <Link href={href} aria-label={`página ${page}`} className={`${className} flex justify-center items-center min-w-[34px] h-[34px] font-bold rounded-md border transition-colors ${isActive ? 'text-contrast bg-active/75 border-transparent' : 'text-secondary text-hover border-primary border-hover'}`}>
+    <Link href={href} aria-label={`página ${page}`} className={`${className} flex justify-center items-center min-w-[34px] h-[34px] font-bold rounded-md border transition-colors ${isActive ? 'text-contrast bg-active/75 border-transparent' : 'text-secondary text-hover border-primary border-hover hover:bg-selected'}`}>
       {page}
     </Link>
   )
