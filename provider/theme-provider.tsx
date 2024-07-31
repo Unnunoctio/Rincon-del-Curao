1f433450072f6317f690bae7933974d1ce519a2b
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-07-31 04:18:23 -0400

    feat: :sparkles: added theme provider

---
+++ provider/theme-provider.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
+import { ThemeProviderProps } from 'next-themes/dist/types'
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, ...props }) => {
+  return (
+    <NextThemeProvider {...props}>
+      {children}
+    </NextThemeProvider>
+  )
+}
