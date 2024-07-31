5e0df3545d23d29a4bc1ca9dcc69a1a660424955
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-07-31 11:06:53 -0400

    feat: :sparkles: added functionality contact form

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -2,37 +2,37 @@
 'use client'
 
 import { useState } from 'react'
-import { Areafield, Emailfield, Textfield } from '../forms/index'
-import { FormInput } from '@/types/types'
 import { SubmitHandler, useForm } from 'react-hook-form'
-// import { sendEmail } from '@/app/actions'
+import { Slide, toast } from 'react-toastify'
+import { Areafield, Emailfield, Textfield } from '../forms'
+import { FormInput } from '@/types/types'
+import { sendEmail } from '@/app/actions'
 
 export const ContactForm: React.FC = () => {
   const [loading, setLoading] = useState(false)
   const { register, handleSubmit, formState: { errors }, reset } = useForm<FormInput>()
 
-  // const successNotify = (): any => toast.success('Mensaje enviado correctamente', {
-  //   containerId: 'notification',
-  //   autoClose: 3000,
-  //   theme: 'colored',
-  //   transition: Slide
-  // })
-  // const errorNotify = (): any => toast.error('Error al enviar el mensaje', {
-  //   containerId: 'notification',
-  //   autoClose: 3000,
-  //   theme: 'colored',
-  //   transition: Slide
-  // })
+  const successNotify = (): any => toast.success('Mensaje enviado correctamente', {
+    containerId: 'notification',
+    autoClose: 3000,
+    theme: 'colored',
+    transition: Slide
+  })
+  const errorNotify = (): any => toast.error('Error al enviar el mensaje', {
+    containerId: 'notification',
+    autoClose: 3000,
+    theme: 'colored',
+    transition: Slide
+  })
 
   const onSubmit: SubmitHandler<FormInput> = async (data) => {
     setLoading(true)
-    // const success = await sendEmail(data)
-    const success = true
+    const success = await sendEmail(data)
     if (success) {
       reset()
-      // successNotify()
+      successNotify()
     } else {
-      // errorNotify()
+      errorNotify()
     }
     setLoading(false)
   }
