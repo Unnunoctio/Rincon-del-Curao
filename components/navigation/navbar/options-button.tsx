ece69ee025aeadd108469e74a0704273b8c37bed
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-05 21:10:47 -0400

    style: :lipstick: updated options button style

--- components/navigation/navbar/options-button.tsx
+++ components/navigation/navbar/options-button.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
 import { GearIcon } from '@/icons'
+import { Popover, PopoverBackdrop, PopoverButton, PopoverPanel } from '@headlessui/react'
 import { ThemeSwitch } from './theme-switch'
+import { WebModal } from '@/components/web-modal'
 
 interface Props {
   onClose: () => void
@@ -10,35 +9,23 @@ interface Props {
 
 export const OptionsButton: React.FC<Props> = ({ onClose }) => {
   return (
-    <Popover className='relative'>
-      {({ open }) => (
-        <>
-          <Popover.Button
-            onClick={onClose}
-            className='group p-2 h-fit rounded-full focus:outline-none'
-            aria-label='opciones'
-          >
-            <GearIcon className={`w-7 h-7 fill-transparent transition-colors icon-group-stroke-hover ${open ? 'stroke-active' : 'icon-stroke-primary'}`} />
-          </Popover.Button>
-          <Popover.Overlay className='fixed inset-0' />
-          <Transition
-            as={Fragment}
-            enter='transition ease-out duration-200'
-            enterFrom='opacity-0 translate-y-1'
-            enterTo='opacity-100 translate-y-0'
-            leave='transition ease-in duration-150'
-            leaveFrom='opacity-100 translate-y-0'
-            leaveTo='opacity-0 translate-y-1'
-          >
-            <Popover.Panel className='absolute left-1/2 z-10 -translate-x-1/2 transform mt-1 px-4 sm:px-0'>
-              <div className='overflow-hidden flex flex-col gap-2 py-3 px-3 bg-primary rounded-lg border divider-primary shadow-lg'>
-                <ThemeSwitch />
-                {/* <SelectedWebsModal allWebs={webs} /> */}
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
+    <Popover className='group'>
+      <PopoverButton
+        onClick={onClose}
+        className='group popover-button'
+        aria-label='opciones'
+      >
+        <GearIcon className='icon-stroke popover-icon' />
+      </PopoverButton>
+      <PopoverBackdrop className='popover-backdrop' />
+      <PopoverPanel
+        transition
+        anchor='bottom'
+        className='popover-panel'
+      >
+        <ThemeSwitch />
+        <WebModal />
+      </PopoverPanel>
     </Popover>
   )
 }
