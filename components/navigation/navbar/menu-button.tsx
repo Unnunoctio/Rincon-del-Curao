ffea1105396a284dd840aec36a1b16a5ad2d81f0
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-05 21:09:26 -0400

    style: :lipstick: updated menu button style

--- components/navigation/navbar/menu-button.tsx
+++ components/navigation/navbar/menu-button.tsx
@@ -8,10 +8,10 @@ export const MenuButton: React.FC<Props> = ({ sideOpen }) => {
   return (
     <button
       onClick={sideOpen}
-      className='group block md:hidden p-2 h-fit rounded-full'
+      className='group navbar-menu-button'
       aria-label='menu'
     >
-      <MenuIcon className='w-7 h-7 fill-transparent transition-colors icon-group-stroke-hover icon-stroke-primary' />
+      <MenuIcon className='icon-stroke' />
     </button>
   )
 }
