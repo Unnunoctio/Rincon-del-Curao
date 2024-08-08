4eea1595ca278b8120e6dd7fc3b487b5632b529a
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-08 11:51:40 -0400

    feat: :sparkles: added simple hash update petitions

---
+++ helpers/hash.ts
@@ -0,0 +1,13 @@
+import { SearchParams } from '@/types/types'
+import { cookies } from 'next/headers'
+
+export const generateHash = (category: string, searchParams: SearchParams): string => {
+  let hash = `${category}`
+
+  const cookieStore = cookies()
+  const webs = cookieStore.get('prefWebs')?.value
+  if (webs !== undefined) hash += '-' + webs.toString()
+
+  if (searchParams.sub_category !== undefined) hash += '-' + searchParams.sub_category.toString()
+  return hash.toLowerCase().replaceAll(' ', '-')
+}
