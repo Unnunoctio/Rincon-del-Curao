1033bb3497e007c2409136cf0635842d08e1c9dc
Unnunoctio <unnunoctio.dev@gmail.com>
2025-02-01 16:53:39 -0300

    fix: :bug: fix url image og

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,14 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Rincón del Curao',
     description: 'Recopilador de precios de distintas bebidas alcohólicas, vendidas en Chile',
-    images: ['hhttps://rincon-del-curao.s3.sa-east-1.amazonaws.com/image-og.png']
+    images: ['https://rincon-del-curao.s3.sa-east-1.amazonaws.com/image-og.png']
   },
   openGraph: {
     title: 'Rincón del Curao',
     description: 'Recopilador de precios de distintas bebidas alcohólicas, vendidas en Chile',
     url: 'https:rincondelcurao.com',
     siteName: 'Rincón del Curao',
-    images: ['hhttps://rincon-del-curao.s3.sa-east-1.amazonaws.com/image-og.png'],
+    images: ['https://rincon-del-curao.s3.sa-east-1.amazonaws.com/image-og.png'],
     locale: 'es_ES',
     type: 'website'
   }
