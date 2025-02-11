631c8653153b95e7585393c32b05f770827ff1b8
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-10 23:05:02 -0300

    feat: :sparkles: add new icons

---
+++ icons/home/point-icon.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  className: string
+}
+
+export const PointIcon: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24' height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0' />
+    </svg>
+  )
+}
