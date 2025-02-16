784b46c15e9950164a6ba60ec07fb5206ced32af
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-16 17:36:05 -0300

    feat: :sparkles: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import tailwindcss from '@tailwindcss/vite';
 import awsAmplify from 'astro-aws-amplify';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()]
   },
+  integrations: [react()],
   adapter: awsAmplify(),
-  output: 'server'
+  output: 'server',
 });
