68e092df5514b02568668565db8ea25e421bdf23
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-16 02:38:38 -0300

    feat: :tada: start project with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import awsAmplify from 'astro-aws-amplify';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()]
+  },
   adapter: awsAmplify(),
   output: 'server'
 });
