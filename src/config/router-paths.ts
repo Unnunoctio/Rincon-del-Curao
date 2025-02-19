d139e14363a94021266797c7e3b47e116f0b4e24
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-19 11:55:40 -0300

    feat: :wrench: add router paths

---
+++ src/config/router-paths.ts
@@ -0,0 +1,53 @@
+export const ROUTES = [
+  {
+    name: 'Cervezas',
+    route: 'beers',
+    icon: 'beer-icon',
+    section: 'beers',
+    categories: [
+      { name: 'Cervezas Artesanales', query: 'Cervezas Artesanales' },
+      { name: 'Cervezas Tradicionales', query: 'Cervezas Tradicionales' },
+      { name: 'Cervezas Importadas', query: 'Cervezas Importadas' },
+      { name: 'Cervezas Sin Alcohol', query: 'Cervezas Sin Alcohol' }
+    ]
+  },
+  {
+    name: 'Vinos',
+    route: 'wines',
+    icon: 'wine-icon',
+    section: 'wines',
+    categories: [
+      { name: 'Vinos Tintos', query: 'Vinos Tintos' },
+      { name: 'Vinos Blancos', query: 'Vinos Blancos' },
+      { name: 'Vinos Rosé', query: 'Vinos Rose' },
+      { name: 'Vinos Cero', query: 'Vinos Cero' }
+    ]
+  },
+  {
+    name: 'Destilados',
+    route: 'distillates',
+    icon: 'distillates-icon',
+    section: 'distillates',
+    categories: [
+      { name: 'Ron', query: 'Ron' },
+      { name: 'Pisco', query: 'Pisco' },
+      { name: 'Vodka', query: 'Vodka' },
+      { name: 'Whisky', query: 'Whisky' }
+    ]
+  }
+]
+
+export const INFO_ROUTES = [
+  {
+    name: 'Términos y Condiciones',
+    route: '/terms'
+  },
+  {
+    name: 'Política de Privacidad',
+    route: '/privacy'
+  },
+  {
+    name: 'Contacto',
+    route: '/contact'
+  }
+]
