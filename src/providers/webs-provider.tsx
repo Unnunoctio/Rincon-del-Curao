3929d71dbb3452b0f7d5347bc090d454113abbbb
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-21 16:19:26 -0300

    feat: :sparkles: add webs provider

---
+++ src/providers/webs-provider.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { WebInfo } from '@/graphql/types'
+import { createContext, useContext } from 'react'
+
+interface WebsContextType {
+  webs: WebInfo[]
+}
+
+const WebsContext = createContext<WebsContextType | undefined>(undefined)
+
+export const useWebs = () => {
+  const context = useContext(WebsContext)
+  if (!context) {
+    throw new Error('useWebs must be used within a WebsProvider')
+  }
+  return context
+}
+
+export const WebsProvider = ({ children, webs }: { children: React.ReactNode, webs: WebInfo[] }) => {
+  return (
+    <WebsContext.Provider value={{ webs }}>
+      {children}
+    </WebsContext.Provider>
+  )
+}
