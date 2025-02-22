2dca841cf87cd659a1b365c834b8bed1edb11df9
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-22 14:29:41 -0300

    feat: :sparkles: add not found page content

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { NotFoundIcon } from "@/icons/not-found"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -6,8 +7,13 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <>
-      <h1 className="text-3xl">404 - NOT FOUND</h1>
-    </>
+    <div className="flex sm:flex-row flex-col-reverse justify-center items-center gap-x-8 gap-y-6 w-full h-full sm">
+      <NotFoundIcon className="hidden sm:block w-[196px] h-[196px]" />
+      <div className="flex flex-col gap-2">
+        <h1 className="font-medium text-[36px]">Oops!</h1>
+        <p className="max-w-[260px] text-[20px] text-c-steel-gray">El contenido que buscas no existe o fue removido.</p>
+        <a href="/" className="mt-1 px-3 py-2 border border-c-steel-gray hover:border-c-snow-white rounded-full w-fit font-medium text-[16px] text-c-steel-gray hover:text-c-snow-white">Ir al inicio</a>
+      </div>
+    </div>
   )
 }
