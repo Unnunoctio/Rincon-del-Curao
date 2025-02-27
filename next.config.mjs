bfde85fe41483e89dbcbf83fa23487b15cb02376
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-02-27 18:46:09 -0300

    feat: :wrench: add images domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.rincondelcurao.com',
+      }
+    ]
+  }
+};
 
 export default nextConfig;
