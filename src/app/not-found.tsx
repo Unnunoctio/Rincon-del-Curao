010ed12fd235f22ad2481744a1369ccf61bbfb45
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-03-02 03:46:19 -0300

    style: :lipstick: change not found text color

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,8 +12,8 @@ export default function NotFoundPage() {
       <NotFoundIcon className="hidden sm:block w-[196px] h-[196px]" />
       <div className="flex flex-col gap-2">
         <h1 className="font-medium text-[36px]">Oops!</h1>
-        <p className="max-w-[260px] text-[20px] text-c-steel-gray">El contenido que buscas no existe o fue removido.</p>
-        <Link href="/" className="mt-1 px-3 py-2 border border-c-steel-gray hover:border-c-snow-white rounded-full w-fit font-medium text-[16px] text-c-steel-gray hover:text-c-snow-white">Ir al inicio</Link>
+        <p className="max-w-[260px] text-[20px] text-c-silver-gray">El contenido que buscas no existe o fue removido.</p>
+        <Link href="/" className="mt-1 px-4 py-2 border border-c-silver-gray hover:border-c-snow-white rounded-full w-fit font-medium text-[16px] text-c-silver-gray hover:text-c-snow-white">Ir al inicio</Link>
       </div>
     </div>
   )
