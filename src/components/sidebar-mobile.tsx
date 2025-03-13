86df1a8667bf5c73b8d7e8674a822caa40748fc7
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2025-03-12 22:17:32 -0300

    style: :lipstick: change sidebar backdrop background

--- src/components/sidebar-mobile.tsx
+++ src/components/sidebar-mobile.tsx
@@ -11,7 +11,7 @@ export const SidebarMobile = () => {
 
   return (
     <Dialog open={isSidebarOpen} onClose={closeSidebar} className="xl:hidden z-50 relative">
-      <DialogBackdrop transition className="fixed inset-0 bg-black/40 data-[closed]:opacity-0 transition-opacity duration-300 ease-in-out" />
+      <DialogBackdrop transition className="fixed inset-0 bg-c-onix-black/40 data-[closed]:opacity-0 transition-opacity duration-300 ease-in-out" />
       <section className="left-0 fixed inset-y-0 flex max-w-[292px] pointer-events-none">
         <DialogPanel transition className="flex flex-col gap-15 bg-c-onix-black shadow-xl p-6 w-screen overflow-y-auto transition-transform data-[closed]:-translate-x-full duration-300 ease-in-out pointer-events-auto transform">
           <DialogTitle className="flex justify-between items-center">
